fd5d71b54c1243e6310a0e548a8de6bcf029aaa6
Haba <91haba@gmail.com>
2020-01-18 12:30:17 +0900

    JPA Auditing (BaseTimeEntity)

--- src/main/java/com/haba/springboot/Application.java
+++ src/main/java/com/haba/springboot/Application.java
@@ -1,11 +1,13 @@
 package com.haba.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 아래 annotation은 스프링 부트의 자동 설정
 // 스프링 Bean 읽기와 생성을 모두 자동으로 해줍니다
 // SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에
 // 이 클래스는 항상 프로젝트의 최상단에 위치해야 합니다
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
