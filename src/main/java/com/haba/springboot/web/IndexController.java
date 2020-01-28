bb78389062d6934280dcbb5bf5923e1fe77cd814
Haba <91haba@gmail.com>
2020-01-28 19:49:48 +0900

    게시글 등록

--- src/main/java/com/haba/springboot/web/IndexController.java
+++ src/main/java/com/haba/springboot/web/IndexController.java
@@ -8,6 +8,15 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
+        //는 index.mustache 를 반환
         return "index";
+    }
+
+    // index.mustache 와 마찬가지로 /posts 를 호출하면, posts-save.mustache를 호출하는 메소드 추가
+    // 파일의 위치는 index.mustache와 같습니다.
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
