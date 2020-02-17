a1f97457eff6fad8a9c56b8479588c1075cce747
Haba <91haba@gmail.com>
2020-02-18 00:12:22 +0900

    5.3 구글 로그인 연동

--- src/main/java/com/haba/springboot/web/IndexController.java
+++ src/main/java/com/haba/springboot/web/IndexController.java
@@ -1,29 +1,38 @@
 package com.haba.springboot.web;
 
+import com.haba.springboot.domain.user.User;
 import com.haba.springboot.service.posts.PostsService;
 import com.haba.springboot.web.dto.PostsResponseDto;
+import com.haba.springboot.config.auth.dto.SessionUser;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         //는 index.mustache 를 반환
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
     // index.mustache 와 마찬가지로 /posts 를 호출하면, posts-save.mustache를 호출하는 메소드 추가
     // 파일의 위치는 index.mustache와 같습니다.
-
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
