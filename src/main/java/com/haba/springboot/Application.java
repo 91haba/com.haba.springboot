e69b7eebef2efd99f5ae47b81210ccf20906c3b4
Haba <91haba@gmail.com>
2020-03-18 23:13:12 +0900

    5.7 기존 테스트에 시큐리 적용 -> 3 failed 왜 실패지..

--- src/main/java/com/haba/springboot/Application.java
+++ src/main/java/com/haba/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // 스프링 Bean 읽기와 생성을 모두 자동으로 해줍니다
 // SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에
 // 이 클래스는 항상 프로젝트의 최상단에 위치해야 합니다
-@EnableJpaAuditing
+// @EnableJpaAuditing가 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
